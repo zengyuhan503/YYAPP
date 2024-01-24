daec23b53ceead9c088e3e92fc2eba82fa81edef
ZENGYUHAN <zengyuhan@skyworth.com>
2024-01-24 15:12:36 +0800

    delete zip

--- YYAPP-master/YYAPP-master/yuyan/src/utils/user/index.ts
+++
@@ -1,9 +0,0 @@
-
-export function logout() {
-  localStorage.removeItem('yuyan-user')
-}
-
-export function loginCms(res) {
-  let tokens = res.token
-  localStorage.setItem('yuyan.token', tokens.token)
-}
